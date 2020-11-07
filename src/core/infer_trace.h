f2d1e379e5cecc6b553b81a4642dc3e997eda649
GuanLuo <gluo@nvidia.com>
2020-11-06 19:38:29 -0800

    Port time related features for Windows

--- src/core/infer_trace.h
+++ src/core/infer_trace.h
@@ -73,9 +73,9 @@ class InferenceTrace {
   // Report trace activity at the current time.
   void ReportNow(const TRITONSERVER_InferenceTraceActivity activity)
   {
-    struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    Report(activity, TIMESPEC_TO_NANOS(ts));
+    Report(activity, std::chrono::duration_cast<std::chrono::nanoseconds>(
+                         std::chrono::steady_clock::now().time_since_epoch())
+                         .count());
   }
 
   // Release the trace. Call the trace release callback and transfer
